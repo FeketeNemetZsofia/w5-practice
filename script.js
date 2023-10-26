ecb76e966038e63b9a77f47a3a7342b9a48c2382
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-10-26 08:38:57 +0200

    yesterday practice

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-console.log("hello")
-document.write("Hello World!")
-const h1Element = document.querySelector('h1')
-h1Element.innerHTML = "Renamed"
 console.log('loaded')
 
 const rootElement = document.querySelector("#root")
-console.log(rootElement)
+console.dir(rootElement)
 
-async function init() { // felkészítjük a js-t h a függvényben lesznek a sync kódok
-    const res = await fetch("https://restcountries.com/v3.1/all") //megvárjuk a fetch válaszát (a promise helyett)
-    const data = await res.json() //megvárjuk h a response megjöjjön promise helyett. Innentől elérhető az adat a változóból
-    console.log(data)
+const countryComponent = (country) => `
+    <div class="country">
+        <h2>country name: ${country.name.common}</h2>
+        <h3>country pop: ${country.population}</h3>
+    </div>
+`
+
+async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+    const response = await fetch("https://restcountries.com/v3.1/all") // megvárjuk a fetch válaszát (promise helyett)
+    const data = await response.json() // megvárjuk, hogy a response megjöjjön (promise helyett) -> itt lesz elérhető az adatunk
+
+    data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
 }
 
 init()
