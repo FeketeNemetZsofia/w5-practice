0a2c8b444c258a6f61fefa95419447989da1c4d6
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-10-26 10:44:33 +0200

    urlfetch function added

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ console.log('loaded')
 const rootElement = document.querySelector("#root")
 console.dir(rootElement)
 
+
+const fetchUrl = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+
 const countryComponent = (country) => `
     <div class="country">
         <h2>country name: ${country.name.common}</h2>
@@ -10,11 +18,19 @@ const countryComponent = (country) => `
     </div>
 `
 
-async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+/*async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
     const response = await fetch("https://restcountries.com/v3.1/all") // megvárjuk a fetch válaszát (promise helyett)
     const data = await response.json() // megvárjuk, hogy a response megjöjjön (promise helyett) -> itt lesz elérhető az adatunk
 
     data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
+}
+
+init()*/
+
+const init = async() => {
+    const finalData = await fetchUrl("https://restcountries.com/v3.1/all")
+    console.log(finalData)
+    finalData.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
 }
 
 init()
