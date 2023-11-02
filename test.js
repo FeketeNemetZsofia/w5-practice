7e7fb8dc542f094435113b01e8c7270edb021e3b
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-11-02 09:37:02 +0100

    end of week

---
+++ test.js
@@ -0,0 +1,8 @@
+let testDate = {year: 2023, month: 12, day: 26 }
+let date = new Date("2018-11-15")
+console.log(date.getDay())
+
+
+
+
+`${region["events"][event].date.year}-${region["events"][event].date.month}-${region["events"][event].date.day}`
